8c4319fa2fbb40ac6ea8b36ba3577b8da963367e
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-04-27 02:06:50 +0530

    add comments

--- client/src/services/serviceWorker.js
+++ client/src/services/serviceWorker.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const BASE_URL = "http://127.0.0.1:5000";
 
+// To get all the attenance data
 export const getAttendaceData = async () => {
     try {
         const task = await axios.get(`${BASE_URL}/`);
@@ -12,6 +13,7 @@ export const getAttendaceData = async () => {
     }
 }
 
+// To recognize the face
 export const getRecognize = async () => {
     try {
         const task = await axios.get(`${BASE_URL}/recognize`);
@@ -23,6 +25,7 @@ export const getRecognize = async () => {
     }
 }
 
+// To insert an attendance to the db
 export const putAttendance = async (recognizedData) => {
     try {
         const task = await axios.post(`${BASE_URL}/attendance`, recognizedData);
@@ -34,6 +37,7 @@ export const putAttendance = async (recognizedData) => {
     }
 }
 
+// To train a new person
 export const giveTraining = async (newPerson) => {
     try {
         const task = await axios.post(`${BASE_URL}/train`, newPerson);
